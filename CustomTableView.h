fe2bfc8f45c2a8cdbcfdaf857b7f05541c8991cc
zaidyes <zaidyes@gmail.com>
2016-06-23 11:40:25 +0200

    Cleaned unwanted code

--- CustomTableView.h
+++ CustomTableView.h
@@ -1,32 +1,11 @@
-// http://qt.developpez.com/doc/4.6/itemviews-frozencolumn/
+// concept from http://qt.developpez.com/doc/4.6/itemviews-frozencolumn/
 
- #ifndef CUSTOM_TABLE_VIEW_H
- #define CUSTOM_TABLE_VIEW_H
-
+#pragma once
 
 //--- include files -------------------------------------------------------------
-#include "Defines.hpp"
 
-#pragma warning_qt_push
 #include <QtGui/QTableView>
 #include <QtCore/QObject>
-#pragma warning_qt_pop
-
-namespace rtt {
-namespace plugin {
-
-// Forward-declaration as GUI invokes backend
-namespace backend {
-
-
-} // namespace backend
-
-namespace data
-{
-
-} // namespace data
-
-namespace ui {
 
 class CustomTableView : public QTableView 
 {
@@ -63,8 +42,3 @@ private:
 	QTableView * m_frozenTvFirst;
 
 };
-
-}
-}
-}
- #endif
